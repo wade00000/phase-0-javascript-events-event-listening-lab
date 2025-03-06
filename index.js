4d5d657bd4cf90e05f3aebb7f489a13b32f0584e
WADE <MEANTOINE12@GMAIL.COM>
2025-03-06 13:26:39 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const input = document.getElementById('button');
+
 function addingEventListener() {
+    input.addEventListener('click', clickAlert);
 }
+
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+
